52a8e888636de0740b631e0d38cf99aabd5f5e35
Lllewisreynolds <lsreynolds108@gmail.com>
2024-01-17 11:47:44 +0000

    "Delete operation added for job listings now that authentication process is working"

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -14,7 +14,30 @@ router.post('/', withAuth, async (req, res) => {
 
         res.status(200).json(newPost);
     } catch (err) {
+        console.log(err);
         res.status(400).json(err);
+    }
+});
+
+// router.delete('/:id', withAuth, async (req, res) => {
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No job listing can be found with this id!' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
