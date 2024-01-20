5d75ba32cfe2f6cde72bfc3bbc12da564238bc88
Tomasz Pawlikowski <tp4458@gmail.com>
2024-01-20 15:09:30 +0000

    login page endpoint added to homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,3 +7,12 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
