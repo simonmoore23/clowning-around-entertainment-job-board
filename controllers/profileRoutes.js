b4cf6a71efcc5c4bea66a817e7115249bdd61b76
Tomasz Pawlikowski <tp4458@gmail.com>
2024-01-22 20:28:03 +0000

    routes to get post by ID and all posts by user

---
+++ controllers/profileRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const { json } = require('sequelize');
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// router.get('/', withAuth, async (req, res) => {
+//   if (!req.session.loggedIn) {
+//     res.render('/');
+//     return;
+//   }
+//   res.render('profile', { loggedIn: req.session.loggedIn });
+// });
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      where: {
+        // use the ID from the session
+        userId: req.session.userId,
+      },
+      attributes: ['id', 'jobTitle', 'description', 'salary', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.render('profile', { posts, loggedIn: true });
+    // })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
