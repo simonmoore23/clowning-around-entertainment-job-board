152c0117a7cc80457f0e3d2fce8a4586c84614e9
Zeus <grahamy2711@yahoo.com>
2024-01-23 21:23:01 +0000

    created delete functions

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -21,26 +21,7 @@ router.post('/', withAuth, async (req, res) => {
 
 // router.delete('/:id', withAuth, async (req, res) => {
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const postData = await Post.destroy({
-      where: {
-        id: req.params.id,
-        userId: req.session.userId,
-      },
-    });
-
-    if (!postData) {
-      res
-        .status(404)
-        .json({ message: 'No job listing can be found with this id!' });
-      return;
-    }
-
-    res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 module.exports = router;
+
+
